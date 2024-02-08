19bf52a819344f69ffbbd212f97dbd6643f0d423
Daniela <danielapb78@gmail.com>
2024-02-08 13:20:01 -0500

    haciendo lab

---
+++ src/app/greetings/greetings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-greetings',
+  templateUrl: './greetings.component.html',
+  styleUrls: ['./greetings.component.css']
+})
+export class GreetingsComponent {
+  
+mensaje: string ='8';
+}
